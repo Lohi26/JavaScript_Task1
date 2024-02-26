9712ba5fa7f42824acdb7eb53bd50f4d73d33e66
Lohi26 <lohitachandru@gmail.com>
2024-02-26 21:49:54 +0530

    stringPractice

--- indexjs.js
+++ indexjs.js
@@ -1,1 +1,22 @@
+document.querySelector("button").addEventListener("click",function(){
+    const text=document.querySelector("textarea").value;
+    let str=text.split("\n");
+    let Sentence="";
+    let count=1;
+    for(let i=0;i<str.length;i++)
+    {
+        str[i]=str[i].trim();
+        let index=str[i].indexOf("_");
+        str[i]=str[i][0].toLowerCase()+str[i].substring(1,index)+str[i][index+1].toUpperCase()+str[i].substring(index+2).toLowerCase();
+        let flag=count;
+        while(flag)
+        {
+            str[i]=str[i].padEnd(20," ")+"✅";
+            flag--;
+        }
+        count++;
+        Sentence+=str[i]+"\n";
+    }
+    document.querySelector("textarea").value=Sentence;
+});
 
